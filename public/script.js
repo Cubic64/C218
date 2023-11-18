1cd7c769244460e89ffda4eafaba376f20e67c05
Cubic64 <95396699+Cubic64@users.noreply.github.com>
2023-11-18 15:54:36 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -18,6 +18,18 @@ navigator.mediaDevices
     .then((stream) => {
         myStream = stream;
         addVideoStream(myVideo, stream);
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) =>{
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        })
     })
 
 function addVideoStream(video, stream){
